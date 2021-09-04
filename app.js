05388126be8c1ec29c7c50f852548d6e079c1c9f
SirBecalo <ahmaddiaa1603@gmail.com>
2021-09-04 14:10:12 +0200

    added svg animations
    incorporated clicks-logic.

--- app.js
+++ app.js
@@ -17,51 +17,77 @@ function randomizer() {
     return randomChoice;
 }
 
-
-//function hadling human input
-function humanChoice() {
-    //ask the user via invasive popup for their tool
-    let humanInput = prompt("Please ener your tool. (rock/paper/scissor)").toLowerCase();
-    //recurse the hell out of wrong inputs intill a proper tool is entered
-    switch (humanInput) {
-        case "rock":
-        case "paper":
-        case "scissor":
-            console.log("you chose: " + humanInput);
-            return mapChoices[humanInput];
-
-        default:
-            console.log("invalid input, please try again.");
-            return humanChoice();
-            
-    }
-
-}
-
 //game logic determining win, loss, draw.
-function logic(computerChoice, humanInput) {
-    /*    c o m p u t e r
-            ---------------
-        h|  
-        u|
-        m|   matrix :)
-        a|
-        n|
-
-    */
+function logic(humanInput, computerChoice) {
+    
     const arrayMatrix = [[0, 2, 1],
                          [1, 0, 2],
                          [2, 1, 0]];
-    let roundState = arrayMatrix[humanInput][computerChoice];
-    return roundState;
+            let result = arrayMatrix[humanInput][computerChoice];
+            console.log("logic result: " + result);
+            return result;
+    
     
 
 }
-//determine length of game
+//hardcoded game length
 function roundsCount() {
-    let rounds = prompt("how many rounds will you endure?");
-    return rounds;
+    return 5;
 }
+
+//the DOM variables:
+let tools = document.querySelectorAll(".tool");
+let rock = document.querySelector("#rock");     
+let paper = document.querySelector("#paper");
+let scissor = document.querySelector("#scissor");
+let divOutput = document.querySelector("#score");
+
+function doRound(e){
+    if(!e.target.closest('.tool')) return;
+    let target = e.target.closest('.tool');
+    let humanChoice = mapChoices[target.id]
+    let randomChoice = randomizer();
+    let result = logic(humanChoice, randomChoice);
+    let roundResult = mapState[result];
+
+
+    switch (roundResult) {
+        case "win":
+            divOutput.innerText = 'What a Glorious Smashing!';
+            break;
+        case "loss":
+            divOutput.innerText = "booooo! lost round!";
+            
+            break;
+        default:
+            divOutput.innerText = "Draw?! :O";
+            break;
+    }
+    return result;
+
+}
+
+divOutput.innerText = "Choose Your Weapon!"
+
+tools.forEach(tool => {
+    tool.addEventListener('mouseover', event => {
+      tool.classList.add("selected");
+    });
+    tool.addEventListener('mouseout', event => {
+        tool.classList.remove("selected");
+      });
+  });
+
+
+  document.addEventListener('click', doRound);
+
+
+
+
+
+
+
+/*
 
 //my main function
 function gameStart() {
@@ -70,13 +96,13 @@ function gameStart() {
     let lossCounter = 0;
     let roundsPlayed = 0;
 
-    //for loop looping each round.
+    //for-looping each round.
 
     for (let gamelength = roundsCount(); gamelength > 0; gamelength--) {
         //get the players' selection
         let humanSelection = humanChoice();
         let computerSelection = randomizer();
-        
+        //print out the round quote
         roundResult = mapState[logic(computerSelection, humanSelection)];
         switch (roundResult) {
             case "win":
@@ -93,6 +119,8 @@ function gameStart() {
         }
         roundsPlayed += 1;
     }
+    
+    //game status
     let state;
     if (lossCounter < winCounter){
         state = "Game Won! HORRAY!";
@@ -104,4 +132,4 @@ function gameStart() {
     }
     console.log("Game Ended. You Won " + winCounter + " rounds." + "\n" + state );
 }
-gameStart();
+*/
